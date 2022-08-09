1934e31d092d3a63be84b1a445dcfd125bd3543e
persianpros <persianpros@yahoo.com>
2022-08-09 15:05:58 +0430

    Setup ovbot
    Co-authored-by: jbleyel <jbleyel@gmx.net>

---
+++ CI/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Script by Persian Prince for https://github.com/OpenVisionE2
+# You're not allowed to remove my copyright or reuse this script without putting this header.
+
+git clone --depth 1 https://gitlab.openpli.org/openpli/hanssettings
+rm -f hanssettings/.gitignore
+rm -f hanssettings/README.md
+cp -rp hanssettings/* .
+rm -rf hanssettings
+
+find . -name '*satellites.xml*' -type f | xargs rm -f
+
+setup_git() {
+  git config --global user.email "bot@openvision.tech"
+  git config --global user.name "Open Vision settings bot"
+}
+
+commit_files() {
+  git checkout master
+  git add -u
+  git add *
+  git commit --message "Fetch latest Hans settings."
+  ./CI/chmod.sh
+  ./CI/dos2unix.sh
+}
+
+upload_files() {
+  git remote add upstream https://${GITHUB_TOKEN}@github.com/OpenVisionE2/Hans-settings.git > /dev/null 2>&1
+  git push --quiet upstream master || echo "failed to push with error $?"
+}
+
+setup_git
+commit_files
+upload_files
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Hans-settings
+Hans-settings [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) [![ovbot](https://github.com/OpenVisionE2/Hans-settings/actions/workflows/ovbot.yml/badge.svg)](https://github.com/OpenVisionE2/Hans-settings/actions/workflows/ovbot.yml)
+=============
 Hans-settings for Open Vision
